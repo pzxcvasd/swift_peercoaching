e27c1b31514bcde9d4b4d28bfa93c8f17c875903
yonsoo-seo <ynsoo.seo@gmail.com>
2024-01-09 19:16:46 +0900

    Initial Commit

---
+++ swift_peercoaching/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  swift_peercoaching
+//
+//  Created by Yeonsu Seo on 1/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swift_peercoaching/swift_peercoachingApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_peercoachingApp.swift
+//  swift_peercoaching
+//
+//  Created by Yeonsu Seo on 1/9/24.
+//
+
+import SwiftUI
+
+@main
+struct swift_peercoachingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
