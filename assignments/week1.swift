2b5975fbf14255b6ab4d055ef858a3056c50d290
yonsoo-seo <ynsoo.seo@gmail.com>
2024-01-09 19:28:16 +0900

    1주차 과제코드

--- assignments/week1.swift
+++ assignments/week1.swift
@@ -1,8 +1,27 @@
-//
-//  week1.swift
-//  swift_peercoaching
-//
-//  Created by Yeonsu Seo on 1/9/24.
-//
+// 1주차 과제 코드
 
-import Foundation
+import SwiftUI
+
+struct week1: View {
+    @State var price = ""
+    @State var selected = "4 people"
+    var people = ["4 people", "5 people", "6 people", "7 people"]
+    var body: some View {
+        NavigationStack {
+            Form{
+                Section {
+                    TextField("Enter the price", text:$price)
+                    Picker("Number of people", selection: $selected) {
+                        ForEach(people,id: \.self) {
+                            Text($0)
+                        }
+                    }
+                }
+            }.navigationTitle("WeSplit")
+        }
+    }
+}
+
+#Preview {
+    week1()
+}
